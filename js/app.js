c8072af0ff7a7db80e829c3de2643cf004e5c95e
Chris Schiebelbein <c_schiebelbein@hotmail.com>
2022-03-18 14:36:32 -0600

    W9B assignment

--- js/app.js
+++ js/app.js
@@ -1,33 +1,43 @@
-var age = 17;
-var is_subbed = true;
+// var age = 17;
+// var is_subbed = true;
 
-// I wasn't sure exactly what you were asking for so I just did it both ways I thought it may need to be done.  Then I just decided to do it a third way in a single if statement cause why not.
+// Perhaps it is just me, but the instructions have felt a bit unclear
+
+//this is assignment W9B, the code was modified for this assignement so check a previous commit for assignment W9A
+var age = [17,19, 25, 15]
+var is_subbed = [true, false, true, false]
+var counter = 0
+
+while (counter < age.length) {
+    if (age[counter] < 18 && is_subbed[counter] == false) {
+        console.log(`User${counter+1} is under 18 and not subbed`)
+    } else if (age[counter] < 18 && is_subbed[counter] == true) {
+        console.log(`User${counter+1}  is subscribed, but under 18`)
+    } else if (age[counter] > 18 && is_subbed[counter] == false ) {
+        console.log(`User${counter+1}  is over 18, but not subscribed`)
+    } else {
+        console.log(`User${counter+1}  is over 18 and subscribed`)
+    }
+    counter++
+}
 
 //Two if else statements for four outputs
-if (age < 18 && is_subbed == false) {
-    console.log('User is under 18 and not subbed')
-} else if (age < 18 && is_subbed == true) {
-    console.log('User is subscribed, but under 18')
-} else if (age > 18 && is_subbed == false ) {
-    console.log('User is over 18, but not subscribed')
-} else {
-    console.log('User is over 18 and subscribed')
-}
+
 
 
 //One if else statement
-if (age < 18) {
-    console.log('Adults Only')
-}
-else if (is_subbed == false) {
-    console.log('Subscription Required')
-} else {
-    console.log('Welcome in')
-} 
+// if (age < 18) {
+//     console.log('Adults Only')
+// }
+// else if (is_subbed == false) {
+//     console.log('Subscription Required')
+// } else {
+//     console.log('Welcome in')
+// } 
 
 //One else statement
-if (age < 18 || is_subbed == false) {
-    console.log('access denied')
-} else {
-    console.log('welcome in')
-}
+// if (age < 18 || is_subbed == false) {
+//     console.log('access denied')
+// } else {
+//     console.log('welcome in')
+// }
